5f131af9f1cbf73c7395553110bab367017e1c5d
Jennifer Fischer <48139253+jenniferprime@users.noreply.github.com>
2023-01-26 21:23:12 +0100

    did a bit more cuz I toats don't have uni early tomorrow

--- nano_sketch/nano_sketch.ino
+++ nano_sketch/nano_sketch.ino
@@ -3,7 +3,8 @@
 const String version = "Ver B0.0.1";
 const bool debug = true;
 
-const int d_reset = 12, d_enable = 11, d_d4 = 5, d_d5 = 4, d_d6 = 3, d_d7 = 2;
+const int d_reset = 12, d_enable = 11, d_d4 = 5, d_d5 = 4, d_d6 = 3, d_d7 = 2,
+          d_colcount = 16, d_rowcount = 2;
 LiquidCrystal lcd(d_reset, d_enable, d_d4, d_d5, d_d6, d_d7);
 
 void setup() {
@@ -21,7 +22,7 @@ void setup() {
 
   lcd.setCursor(0,0);
   Serial.setTimeout(16);
-  lcd.print(version+" T: 16ms");
+  lcd.print(version+" t16ms");
   lcd.setCursor(0, 1);
   lcd.print("Wait on host");
 
@@ -35,22 +36,32 @@ void loop() {
   {
     if(debug) { Serial.println("rec"); }
 
-    String command = Serial.readString();
+    String command = Serial.readStringUntil('~');
+    Serial.flush();
     if(debug) {Serial.println(command);}
 
     char action = command[0];
     if(debug) {Serial.println(action);}
 
-    lcd.clear();
-    lcd.setCursor(0, 0);
     
     switch(action)
     {
-      case 'f':
-          lcd.print("haha hey");
+      case 'r': // raw text entry
+          lcd.clear();
+          lcd.println(command.substring(1,17));
+          lcd.setCursor(0, 1);
+          lcd.println(command.substring(17,command.length()-1));// TODO: fix the issue that this has to be padded and general slowness
           break;
-        case 'b':
-          lcd.print("haha no");
+        case 't': // raw text entry for top line
+          lcd.setCursor(0, 0);
+          lcd.print(command.substring(1, command.length()-1));
+          break;
+        case 'b': // raw text entry for bottom line //TODO: make this one command that also gets an int
+          lcd.setCursor(0, 1);
+          lcd.print(command.substring(1, command.length()-1));
+          break;
+        case 'c': // clear command
+          lcd.clear();
           break;
         default: 
           lcd.print("invalid input");
